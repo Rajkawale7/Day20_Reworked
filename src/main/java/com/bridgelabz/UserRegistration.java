b313f34a545c9888ba8f7c3fc5beb19902931004
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-16 14:31:45 +0530

    Added: Fourth Test Case to check and validate User's Contact Number

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,18 +5,17 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static final Scanner scanner = new Scanner(System.in);
 
-    public static boolean validEmail() {
-        System.out.print("Enter the EmailID : ");
-        String email = scanner.next();
-        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        Pattern p3 = Pattern.compile(regex);
-        Matcher m3 = p3.matcher(email);
-        boolean r3 = m3.matches();
-
-        if (r3)
-            System.out.println("Email_ID is valid");
+    public static boolean validMobileNumber() {
+        System.out.print("Enter your mobile number : ");
+        String phoneNo = scanner.nextLine();
+        String regex4 = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
+        Pattern p4 = Pattern.compile(regex4);
+        Matcher m4 = p4.matcher(phoneNo);
+        boolean r4 = m4.matches();
+        if (r4)
+            System.out.println("Phone number is Valid");
         else
-            System.out.println("Email_ID is !Invalid");
-        return r3;
-     }
+            System.out.println("Phone number is Invalid");
+        return r4;
+    }
 }
--- src/test/java/com/bridgelabz/UserRegisterationTest.java
+++ src/test/java/com/bridgelabz/UserRegisterationTest.java
@@ -3,20 +3,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class UserRegisterationTest {
 
-        //Test cases to check and validate the Email must be "True"
+        //Test cases to check and validate the Contact Number must be "True"
 
         @Test
-        public void givenEmail_WhenProper_ShouldReturnTrue() {
+        public void givenContact_WhenProper_ShouldReturnTrue() {
             UserRegistration userRegistration = new UserRegistration();
-            boolean result= userRegistration.validEmail();
-            Assertions.assertTrue(result);
+            boolean result4 = userRegistration.validMobileNumber();
+            Assertions.assertTrue(result4);
         }
 
         //Test cases to check and validate the Email must be "False"
         @Test
         public void givenEmail_WhenProper_ShouldReturnFalse() {
             UserRegistration userRegistration=new UserRegistration();
-            boolean result= userRegistration.validEmail();
-            Assertions.assertFalse(result);
+            boolean result4 = userRegistration.validMobileNumber();
+            Assertions.assertFalse(result4);
         }
 }
